e94a4a22f9c75690b93fc9d780a96866ac30d1c0
Antony Blakey <antony.blakey@gmail.com>
2022-04-09 17:56:48 +0200

    Multi-arch build and docker hub push

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-$TAG=$1
+TAG=$1
 
 docker push linkuistics/devanyware-headless:${TAG}
 docker push linkuistics/devanyware-headfull:${TAG}
--- tag.sh
+++ tag.sh
@@ -3,7 +3,4 @@
 TAG=$1
 
 docker tag linkuistics/devanyware-headless linkuistics/devanyware-headless:$TAG
-docker rmi linkuistics/devanyware-headless:latest
-
 docker tag linkuistics/devanyware-headfull linkuistics/devanyware-headfull:$TAG
-docker rmi linkuistics/devanyware-headfull:latest
